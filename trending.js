7d857b33ba57f813e2b58a02c3162085f8db3b3e
pareshbhaic56-code <pareshbhaic56@gmail.com>
2026-07-17 01:09:21 +0530

    Create trending.js

---
+++ trending.js
@@ -0,0 +1,21 @@
+function loadTrending(){
+
+document.getElementById("trendingOutput").innerHTML=
+
+"<h2>🔥 Today's Trending Topics</h2>" +
+
+"<ul>" +
+
+"<li>AI Business Ideas</li>" +
+
+"<li>How To Earn Money Online</li>" +
+
+"<li>ChatGPT Tips</li>" +
+
+"<li>YouTube Automation</li>" +
+
+"<li>Best AI Tools 2026</li>" +
+
+"</ul>";
+
+}
