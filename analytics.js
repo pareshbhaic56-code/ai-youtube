7101fed91243c67dd11abd6b0f695fd5d57d8601
pareshbhaic56-code <pareshbhaic56@gmail.com>
2026-07-17 01:04:38 +0530

    Add loadAnalytics function to display demo data

---
+++ analytics.js
@@ -0,0 +1,11 @@
+function loadAnalytics(){
+
+document.getElementById("views").innerHTML="125,430";
+
+document.getElementById("subs").innerHTML="8,250";
+
+document.getElementById("videos").innerHTML="57";
+
+alert("Demo Analytics Loaded");
+
+}
