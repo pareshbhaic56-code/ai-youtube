473fad06d4f85e0140bf94290cfa86a7e8efbed7
pareshbhaic56-code <pareshbhaic56@gmail.com>
2026-07-17 01:14:12 +0530

    Add GeminiAPI with script generation methods

---
+++ gemini-api.js
@@ -0,0 +1,23 @@
+const GeminiAPI={
+
+apiKey:"YOUR_GEMINI_API_KEY",
+
+async generateScript(topic){
+
+return "Demo AI Script for: "+topic;
+
+},
+
+async generateTitle(topic){
+
+return "Best "+topic+" Video";
+
+},
+
+async generateDescription(topic){
+
+return "This is a demo description for "+topic;
+
+}
+
+};
