3c299d0efc2f73b961b1d9f9c1b039e005445146
pareshbhaic56-code <pareshbhaic56@gmail.com>
2026-07-16 17:36:59 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function scriptAI(){
+
+alert("AI Script Generator Coming Soon");
+
+}
+
+function videoAI(){
+
+alert("AI Video Generator Coming Soon");
+
+}
+
+function thumbnailAI(){
+
+alert("AI Thumbnail Generator Coming Soon");
+
+}
+
+function uploadAI(){
+
+alert("YouTube Upload Coming Soon");
+
+}
